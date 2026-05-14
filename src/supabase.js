639484262ec331963cb8cf3b97b4504c9c4035ea
sitevanessarafaella <sitevanessarafaella@gmail.com>
2026-05-14 03:50:57 -0300

    fix: hardcode supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://vnqqrssamfddalitchry.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZucXFyc3NhbWZkZGFsaXRjaHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTY2NDIsImV4cCI6MjA5NDI5MjY0Mn0.cj57Mt9vsWPzqZEliAyR0agXrNbBoYRRlS5CoQVmA-I'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
